f4ca17688bc6d174ee2c724f07a92e7db4484e5f
Srivas Chennu <srivas@gmail.com>
2018-06-19 00:00:27 +0100

    Checked in Tue Jun 19 00:00:27 BST 2018

--- plotfigures.m
+++ plotfigures.m
@@ -1,17 +1,22 @@
 plottrend('frontiers','ftdwpli',{'participation coefficient'},3,'patient','p10','ylim',[-20 90],'ylabel','\Delta \sigma(\alpha Part. Coef.) (%)','relative','ratio')
 plottrend('frontiers','ftdwpli',{'crs','auditory','visual','motor','verbal','communication','arousal'},3,'patient','p10','ylim',[0 9],'ylabel','CRS-R Score')
+plottrend('frontiers','ftdwpli',{'power'},1,'patient','p10','ylim',[-40 20],'ylabel','\Delta (Norm. \delta Power) (%)','relative','percent');
 
 plottrend('frontiers','ftdwpli',{'participation coefficient'},3,'patient','p21','ylim',[-20 90],'ylabel','\Delta \sigma(\alpha Part. Coef.) (%)','relative','ratio')
 plottrend('frontiers','ftdwpli',{'crs','auditory','visual','motor','verbal','communication','arousal'},3,'patient','p21','ylim',[0 9],'ylabel','CRS-R Score');
+plottrend('frontiers','ftdwpli',{'power'},1,'patient','p21','ylim',[-40 20],'ylabel','\Delta (Norm. \delta Power) (%)','relative','percent');
 
 plottrend('frontiers','ftdwpli',{'power'},1,'patient','p03','ylim',[-40 20],'ylabel','\Delta (Norm. \delta Power) (%)','relative','percent');
 plottrend('frontiers','ftdwpli',{'crs','auditory','visual','motor','verbal','communication','arousal'},3,'patient','p03','ylim',[0 16],'ylabel','CRS-R Score')
+plottrend('frontiers','ftdwpli',{'participation coefficient'},3,'patient','p03','ylim',[-20 90],'ylabel','\Delta \sigma(\alpha Part. Coef.) (%)','relative','ratio')
 
 plottrend('frontiers','ftdwpli',{'power'},1,'patient','p18','ylim',[-40 20],'ylabel','\Delta (Norm. \delta Power) (%)','relative','percent');
 plottrend('frontiers','ftdwpli',{'crs','auditory','visual','motor','verbal','communication','arousal'},3,'patient','p18','ylim',[0 16],'ylabel','CRS-R Score')
+plottrend('frontiers','ftdwpli',{'participation coefficient'},3,'patient','p18','ylim',[-20 90],'ylabel','\Delta \sigma(\alpha Part. Coef.) (%)','relative','ratio')
 
-plottrend('frontiers','ftdwpli',{'participation coefficient'},1,'patient','p28','ylim',[-70 30],'ylabel','\Delta \sigma(\delta Part. Coef.) (%)','relative','ratio');
-plottrend('frontiers','ftdwpli',{'crs','auditory','visual','motor','verbal','communication','arousal'},3,'patient','p28','ylim',[0 23],'ylabel','CRS-R Score');
+
+% plottrend('frontiers','ftdwpli',{'participation coefficient'},1,'patient','p28','ylim',[-70 30],'ylabel','\Delta \sigma(\delta Part. Coef.) (%)','relative','ratio');
+% plottrend('frontiers','ftdwpli',{'crs','auditory','visual','motor','verbal','communication','arousal'},3,'patient','p28','ylim',[0 23],'ylabel','CRS-R Score');
 
 % plottrend('frontiers','ftdwpli',{'participation coefficient'},3,'patient','p28','ylim',[-20 90],'ylabel','\Delta \sigma(\alpha Part. Coef.) (%)','relative','ratio')
 % plottrend('frontiers','ftdwpli',{'power'},1,'patient','p28','ylim',[-40 20],'ylabel','\Delta (Norm. \delta Power) (%)','relative','percent');
